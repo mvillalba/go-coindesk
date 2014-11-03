277fa377380f356d73e1cc29ae6f3af39cc9f1c7
Martín Raúl Villalba <martin@martinvillalba.com>
2014-11-04 02:00:13 +0700

    Add version information.

---
+++ bpi/version.go
@@ -0,0 +1,20 @@
+package bpi
+
+import "fmt"
+
+var (
+    VersionMajor = 0
+    VersionMinor = 1
+    VersionRevision = 0
+    VersionTag = "dev"
+    Version = fmt.Sprintf("%v.%v.%v", VersionMajor, VersionMinor, VersionRevision)
+    AuthorName = "Martín Raúl Villalba"
+    AuthorEMail = "martin@martinvillalba.com"
+    Author = fmt.Sprintf("%v <%v>", AuthorName, AuthorEMail)
+)
+
+func init() {
+    if VersionTag != "" {
+        Version = fmt.Sprintf("%v-%v", Version, VersionTag)
+    }
+}
